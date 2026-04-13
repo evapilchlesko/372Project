54b7cfbeb273a339562650fefcdb4fc113da4000
JoureFadhil <jourefadhil@arizona.edu>
2026-04-13 13:22:32 -0700

    Update project12.jl

--- project12.jl
+++ project12.jl
@@ -1,1 +1,28 @@
+import Pkg; Pkg.add("Images")
 
+img_array = load("coins.ascii.pgm")
+using Random
+using Statistics
+
+
+function main()
+   
+    iterations=1
+    g1=[] #group 1
+    g2=[] #group 2
+    vals = vec(img_array)
+    sample = rand(vals, 10)
+    T = mean(sample) #this will be updated by the new g1+g2/2
+    for num in vec
+        if num<T 
+            
+    end 
+
+
+
+    #println(vals)
+    # println("Doing work...")
+ end
+
+# Call the function at the bottom of the script
+main()
